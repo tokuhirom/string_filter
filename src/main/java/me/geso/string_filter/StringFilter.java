96a3b692785079b87b4f02ddb2b8439078d2d4c2
Tokuhiro Matsuno <tokuhirom@gmail.com>
2016-05-03 12:13:03 +0900

    initial import

---
+++ src/main/java/me/geso/string_filter/StringFilter.java
@@ -0,0 +1,92 @@
+package me.geso.string_filter;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class StringFilter {
+    private final Map<String, BiFunction<String, Matcher, String>> rules;
+    private final Function<String, String> defaultRule;
+    private final Pattern regexp;
+
+    public static StringFilterBuilder builder() {
+        return new StringFilterBuilder();
+    }
+
+    public StringFilter(Map<String, BiFunction<String, Matcher, String>> rules, Function<String, String> defaultRule) {
+        this.rules = rules;
+        this.defaultRule = defaultRule;
+        this.regexp = Pattern.compile("(" + rules.keySet().stream().collect(Collectors.joining("|")) + ")");
+    }
+
+    public String filter(String input) {
+        int index = 0;
+
+        ArrayList<String> matchList = new ArrayList<>();
+        Matcher m = regexp.matcher(input);
+
+        // Add segments before each match found
+        while (m.find()) {
+            if (index == 0 && index == m.start() && m.start() == m.end()) {
+                // no empty leading substring included for zero-width match
+                // at the beginning of the input char sequence.
+                continue;
+            }
+            String match = input.subSequence(index, m.start()).toString();
+            matchList.add(defaultRule.apply(match));
+            matchList.add(processMatched(m.group()));
+            index = m.end();
+        }
+
+        // If no match was found, return this
+        if (index == 0) {
+            return input;
+        }
+
+        // Add remaining segment
+        matchList.add(defaultRule.apply(input.subSequence(index, input.length()).toString()));
+
+        return matchList.stream().collect(Collectors.joining(""));
+    }
+
+    private String processMatched(String input) {
+        for (Map.Entry<String, BiFunction<String, Matcher, String>> rule : rules.entrySet()) {
+            // TODO: precompile
+            Pattern pattern = Pattern.compile(rule.getKey());
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches()) {
+                return rule.getValue().apply(input, matcher);
+            }
+        }
+        throw new IllegalStateException("Unmatched pattern: '" + input + "'");
+    }
+
+    public static class StringFilterBuilder {
+        private final Map<String, BiFunction<String, Matcher, String>> rules;
+        private Function<String, String> defaultRule;
+
+        StringFilterBuilder() {
+            rules = new LinkedHashMap<>();
+            defaultRule = str -> str;
+        }
+
+        public StringFilterBuilder addRule(String pattern, BiFunction<String, Matcher, String> callback) {
+            rules.put(pattern, callback);
+            return this;
+        }
+
+        public StringFilterBuilder defaultRule(Function<String, String> defaultRule) {
+            this.defaultRule = defaultRule;
+            return this;
+        }
+
+        public StringFilter build() {
+            return new StringFilter(rules, defaultRule);
+        }
+    }
+}
---
+++ src/test/java/me/geso/string_filter/StringFilterTest.java
@@ -0,0 +1,56 @@
+package me.geso.string_filter;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.junit.Test;
+
+import static org.apache.commons.lang3.StringEscapeUtils.escapeHtml4;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tokuhirom on 5/3/16.
+ */
+public class StringFilterTest {
+    @Test
+    public void filterNop() throws Exception {
+        StringFilter filter = StringFilter.builder()
+                .build();
+        assertEquals(filter.filter("hoge"), "hoge");
+    }
+
+    @Test
+    public void filterSimple() throws Exception {
+        // 第一引数の s いらない疑惑
+        StringFilter filter = StringFilter.builder()
+                .addRule("http://[A-Za-z0-9_\\-\\~\\.\\%\\?\\#\\@/]+", (s, m) -> {
+                    String url = m.group(0);
+
+                    return "<a href=\""
+                            + escapeHtml4(url) +
+                            "\">" + escapeHtml4(url) + "</a>";
+                })
+                .addRule("((?:^|\\s)@)([A-Za-z0-9_]+)", (s, m) -> {
+                    String prefix = m.group(1);
+                    String user = m.group(2);
+
+                    return prefix + "<a href=\"http://twitter.com/"
+                            + escapeHtml4(user) +
+                            "\">" + escapeHtml4(user) + "</a>";
+                })
+                .addRule("((?:^|\\s))(#[A-Za-z0-9_]+)", (s,m) -> {
+                    String prefix = m.group(1);
+                    String hashtag = m.group(2);
+                    return prefix + "<a href=\"http://twitter.com/search?q="
+                             + escapeHtml4(hashtag)
+                            + "\">"
+                            + escapeHtml4(hashtag)
+                            + "</a>";
+                })
+                .defaultRule(StringEscapeUtils::escapeHtml4)
+                .build();
+
+        assertEquals("aaa @<a href=\"http://twitter.com/hoge\">hoge</a> bbb", filter.filter("aaa @hoge bbb"));
+        assertEquals("aaa <a href=\"http://twitter.com/search?q=#hoge\">#hoge</a> bbb&gt;", filter.filter("aaa #hoge bbb>"));
+        assertEquals("&lt; <a href=\"http://mixi.jp/\">http://mixi.jp/</a> bbb&gt;", filter.filter("< http://mixi.jp/ bbb>"));
+    }
+
+}
